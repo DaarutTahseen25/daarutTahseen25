7cd9098a83701457ee4add130d140f3cffdc8d99
Gifton Otieno <otienogifton2004@gmail.com>
2025-06-19 15:40:35 +0300

    set up the base styles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,39 @@
-import React from 'react'
-import './App.css'
+import React from "react";
+import Button from "./components/Button";
+import Card from "./components/Card";
 
 function App() {
-
-
   return (
     <>
-      <div>
-        <h1 className="text-3xl font-bold underline">
+      <div className="bg-secondary h-screen">
+        <h1 className="text-3xl font-bold underline text-success">
           Hello world!
         </h1>
-        <p className="text-lg text-gray-700">
+        <p className="text-lg text-textmain">
           This is a simple React app with Tailwind CSS.
         </p>
-        <button className="bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-700 transition duration-300">
+        <p className="text-textmuted">
+          all the custom colors are specified in the index.css file
+        </p>
+        <button className="bg-primary hover:bg-buttonhover text-white font-bold py-2 px-4 rounded hover:bg-blue-700 transition duration-300">
           Click Me
         </button>
+        <br />
+        <br />
+        <Button variant="primary">Primary button</Button>
+        <Card
+          title="Welcome to DaarutTahseen"
+          description="Here’s a quick overview of your activity"
+          footer={<Button variant="primary">Continue</Button>}
+        >
+          <p>
+            This is the body of the card. You can pass in anything here — text,
+            lists, charts, etc.
+          </p>
+        </Card>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
