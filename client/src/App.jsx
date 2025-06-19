bc84327c6987b320fd5c9081bd8f3a10a346a69d
lawalemma24 <lawalemma24@gmail.com>
2025-06-19 08:11:51 +0100

    work setup

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+        <p className="text-lg text-gray-700">
+          This is a simple React app with Tailwind CSS.
+        </p>
+        <button className="bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-700 transition duration-300">
+          Click Me
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
