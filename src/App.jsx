a15050088cfb8e54dc0959fe328d042b57e70b94
Bello Qudus Folohunsho <qudusalabi19@gmail.com>
2025-06-20 06:00:35 +0100

    feat(landing): design the last three sections of the landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 // App.jsx
 import { Routes, Route, Link } from "react-router";
-import Home from "./pages/Home";
+import Home from "./pages/home";
 import About from "./pages/About";
 import "./App.css";
 
 const App = () => {
   return (
     <>
-      <nav>
-        <Link to="/">Home</Link> | <Link to="/about">About</Link>
-      </nav>
-
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
