089c761322d84656c28b79bbf962081917822da0
Bello Qudus Folohunsho <qudusalabi19@gmail.com>
2025-06-22 18:09:54 +0100

    fix: Change hero text font and header logo

--- src/Components/HeroInfo.jsx
+++ src/Components/HeroInfo.jsx
@@ -7,13 +7,13 @@ const HeroInfo = () => {
   const user = "";
   return (
     <div className="flex-1 flex flex-col gap-6 w-full lg:w-auto max-w-full ">
-      <h1 className="text-primary font-abril text-[28px] sm:text-[36px] md:text-[42px] font-normal leading-snug tracking-wide  text-center lg:text-left w-full">
+      <h1 className="text-primary font-poppins text-[28px] sm:text-[36px] md:text-[41px] font-normal leading-snug tracking-wide  text-center lg:text-left w-full">
         DaarutTahseen Institution{" "}
         <span className="text-accent">brings Authentic </span>
         Islamic Education
         <span className="text-accent"> to Every Home, Digitally</span>
       </h1>
-      <p className="text-accent font-bricolage text-[16px] sm:text-[18px] md:text-[20px] leading-relaxed max-w-full sm:max-w-[534px] text-center lg:text-left w-full sm:mx-auto ">
+      <p className="text-accent font-bricolage text-[16px] sm:text-[18px] md:text-[20px] leading-relaxed max-w-full sm:max-w-[534px] text-center w-full sm:mx-auto lg:mx-0 lg:text-left  ">
         DaarutTahseen Institution is a trusted online madrassah where students
         across Nigeria and beyond receive structured Islamic education,
         anywhere, anytime.
@@ -21,7 +21,7 @@ const HeroInfo = () => {
 
       {/* Shows get started when there's no logged in user and go to dashboard if a user is logged in */}
       <div className="self-center lg:self-start">
-        {!user ? (
+        {user ? (
           <Button className="bg-primary hover:bg-buttonhover transition-colors duration-300 py-4 px-6 w-[219px] h-[71px] text-white text-[18px] font-medium rounded-[10px] border-2 border-cream shadow-[0_0_10px_5px_rgba(0,0,0,0.25)] font-clash">
             Go to Dashboard
           </Button>
