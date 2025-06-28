752263dd74dd11fd365c6e45f66f650ba7d79130
lawalemma24 <lawalemma24@gmail.com>
2025-06-28 06:56:09 +0100

    set up the login page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Routes, Route, Link } from "react-router";
 import Home from "./pages/home";
 import About from "./pages/About";
 import "./App.css";
+import LogIn from "./pages/LogIn";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/login" element={<LogIn />} />
       </Routes>
     </>
   );
