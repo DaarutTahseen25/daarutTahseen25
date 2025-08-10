d81524d5de4dd2783f3b86ce5fc180b4b447b88d
Bello Qudus Folohunsho <qudusalabi19@gmail.com>
2025-08-10 17:41:55 +0100

    style: Update JSX formatting and class names for consistency in MyCourses component

--- src/pages/student/MyCourses.jsx
+++ src/pages/student/MyCourses.jsx
@@ -187,52 +187,49 @@ export default function MyCourses() {
     );
 
   return (
-    <div className="p-4 min-h-screen font-clash">
-      <div className="mb-4">
-        <h1 className="text-3xl md:text-4xl font-semibold text-accent">
+    <div className='p-4 min-h-screen font-clash'>
+      <div className='mb-4'>
+        <h1 className='text-3xl md:text-4xl font-semibold text-accent'>
           My Courses
         </h1>
 
         {/* Filters */}
-        <div className="flex flex-wrap items-center gap-3 mt-4 bg-white max-w-3xl px-3 py-2 rounded-lg border border-textmuted">
-          <button className="flex items-center px-3 py-1 text-textmain">
-            <Filter size={16} className="mr-2" /> Filter by
+        <div className='flex flex-wrap items-center gap-3 mt-4 bg-white max-w-3xl px-3 py-2 rounded-lg border border-textmuted'>
+          <button className='flex items-center px-3 py-1 text-textmain'>
+            <Filter size={16} className='mr-2' /> Filter by
           </button>
           <select
-            className="px-2 py-1 text-sm border-l border-textmuted text-textmain"
+            className='px-2 py-1 text-sm border-l border-textmuted text-textmain'
             value={filterProgress}
-            onChange={(e) => setFilterProgress(e.target.value)}
-          >
-            <option value="">Date</option>
-            <option value="Progress">Progress</option>
+            onChange={(e) => setFilterProgress(e.target.value)}>
+            <option value=''>Date</option>
+            <option value='Progress'>Progress</option>
           </select>
           <select
-            className="px-2 py-1 text-sm border-l border-textmuted text-textmain"
+            className='px-2 py-1 text-sm border-l border-textmuted text-textmain'
             value={filterStatus}
-            onChange={(e) => setFilterStatus(e.target.value)}
-          >
-            <option value="">Status</option>
-            <option value="Completed">Completed</option>
-            <option value="In progress">In progress</option>
+            onChange={(e) => setFilterStatus(e.target.value)}>
+            <option value=''>Status</option>
+            <option value='Completed'>Completed</option>
+            <option value='In progress'>In progress</option>
           </select>
           <input
-            type="text"
-            placeholder="Search..."
+            type='text'
+            placeholder='Search...'
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
-            className="border px-2 py-1 rounded text-sm shadow-sm border-textmuted focus:outline-none focus:ring-1 focus:ring-primary"
+            className='border px-2 py-1 rounded text-sm shadow-sm border-textmuted focus:outline-none focus:ring-1 focus:ring-primary'
           />
           <button
             onClick={resetFilters}
-            className="text-red-400 text-sm flex items-center gap-1"
-          >
-            <RefreshCcw className="w-5 h-5" />
+            className='text-red-400 text-sm flex items-center gap-1'>
+            <RefreshCcw className='w-5 h-5' />
             Reset
           </button>
         </div>
 
         {/* Tabs */}
-        <div className="flex flex-wrap justify-between items-center mt-6 gap-4 text-sm sm:text-base md:text-xl border-b overflow-x-auto">
+        <div className='flex flex-wrap justify-between items-center mt-6 gap-4 text-sm sm:text-base md:text-xl border-b overflow-x-auto'>
           {["Classes", "Total Courses", "Assignment", "Quiz"].map((tab) => (
             <button
               key={tab}
@@ -241,8 +238,7 @@ export default function MyCourses() {
                 activeTab === tab
                   ? "text-green-600 border-b-2 border-green-600"
                   : ""
-              }`}
-            >
+              }`}>
               {tab}
             </button>
           ))}
@@ -253,13 +249,13 @@ export default function MyCourses() {
 
       {activeTab === "Total Courses" && (
         <>
-          <div className="bg-white w-full p-4 rounded-lg shadow overflow-auto">
-            <div className="p-2 min-w-3xl mx-auto space-y-4">
-              <div className="grid grid-cols-4 gap-4 text-md md:text-xl py-2 px-3 md:py-3 mb-4 bg-light-grey mb-2">
-                <div className="font-semibold">Course Name</div>
-                <div className="font-semibold">Progress</div>
-                <div className="font-semibold">Overall Score</div>
-                <div className="font-semibold">Status</div>
+          <div className='bg-white w-full p-4 rounded-lg shadow overflow-auto'>
+            <div className='p-2 min-w-3xl mx-auto space-y-4'>
+              <div className='grid grid-cols-4 gap-4 text-md md:text-xl py-2 px-3 md:py-3 mb-4 bg-light-grey'>
+                <div className='font-semibold'>Course Name</div>
+                <div className='font-semibold'>Progress</div>
+                <div className='font-semibold'>Overall Score</div>
+                <div className='font-semibold'>Status</div>
               </div>
               <TotalCourses
                 courses={filteredCourses}
@@ -272,8 +268,8 @@ export default function MyCourses() {
       )}
 
       {activeTab === "Quiz" && quizList.length > 0 && (
-        <div className="w-full overflow-x-hidden">
-          <div className="grid grid-cols-1 md:grid-cols-2 [@media(min-width:1201px)]:grid-cols-3 gap-3 w-full">
+        <div className='w-full overflow-x-hidden'>
+          <div className='grid grid-cols-1 md:grid-cols-2 [@media(min-width:1201px)]:grid-cols-3 gap-3 w-full'>
             {quizList.map((quiz, idx) => (
               <QuizCardComponent key={idx} {...quiz} />
             ))}
