669a8f189572cc0f431edfb15501b1f598454e37
Bello Qudus Folohunsho <qudusalabi19@gmail.com>
2025-12-02 22:38:41 +0100

    Update Loader component styling

--- src/Components/Loader.jsx
+++ src/Components/Loader.jsx
@@ -1,7 +1,7 @@
 export const Loader = () => {
   return (
     <div className="loader-container">
-      <div className="loader"></div>
+      <div className="h-10 w-10 animate-spin rounded-full border-4 border-gray-300 border-t-primary"></div>
     </div>
   );
 };
